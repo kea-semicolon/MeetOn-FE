00188456c73cb93edd4f5cfd0e36c22f71f36683
은지 <81403265+eunji0714@users.noreply.github.com>
2024-03-27 12:29:59 +0900

    KEA-83 refactor: get token from server

--- src/app/(route)/login/kakao/page.tsx
+++ src/app/(route)/login/kakao/page.tsx
@@ -2,30 +2,28 @@
 
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 const LoginHandler = () => {
-  const [code, setCode] = useState(null)
+  const router = useRouter()
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const codeURL = new URL(window.location.href).searchParams.get('code')
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      setCode(codeURL)
       if (codeURL) {
         axios
-          .post('http://localhost:8080/oauth/callback/kakao', { code: codeURL })
+          .post(`http://localhost:8080/oauth/callback/kakao?code=${codeURL}`)
           .then((response) => {
             // 요청이 성공할 경우 토큰을 받아옴
-            const { token } = response.data
-            localStorage.setItem('accessToken', token)
+            localStorage.setItem('accessToken', response.data.accessToken)
+            localStorage.setItem('refreshToken', response.data.refreshToken)
+            router.push('/main')
           })
           .catch((error) => {
             console.error('Error fetching token:', error)
           })
       }
     }
-  }, [])
-  return <div>{code && <p>{code}</p>}</div>
+  }, [router])
 }
 
 export default LoginHandler
--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -2,10 +2,10 @@
 
 export default function Login() {
   const handleLogin = () => {
-    window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=&redirect_uri=http://localhost:3000/login/kakao`
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=1bab02deaaf8c73caa95dcdd8e84f6b7&redirect_uri=http://localhost:3000/login/kakao`
   }
   return (
-    <div className="flex flex-col bg-gray-200 h-screen">
+    <div className="flex flex-col">
       <button
         type="button"
         onClick={handleLogin}
