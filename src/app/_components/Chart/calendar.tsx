54576aa06da3e9d78698f591d7f30d12511c00d7
maylh <gus_05@naver.com>
2024-03-30 19:09:22 +0900

    KEA-85 feat: Add FullCalendar component with basic setup

--- src/app/_components/Chart/calendar.tsx
+++ src/app/_components/Chart/calendar.tsx
@@ -1,5 +1,44 @@
-const Calendar = () => {
-  return <div>Calendar Test 2</div>
-}
+'use client'
 
-export default Calendar
+import { useState } from 'react'
+import { NextPage } from 'next'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+const Calendar: NextPage = () => {
+    const [showModal, setShowModal] = useState(false)
+    const [content, setContent] = useState('')
+    const [startDate, setStartDate] = useState('')
+    const [endDate, setEndDate] = useState('')
+    const [events, setEvents] = useState([])
+
+    return (
+        <div>
+            <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin]}
+                initialView="dayGridMonth"
+                timeZone="UTC"
+                aspectRatio={2}
+                headerToolbar={{
+                    left: 'addEventButton',
+                    center: 'title'
+                }}
+                customButtons={{
+                    addEventButton: {
+                        text: '일정 추가',
+                        click: () => setShowModal(true),
+                    },
+                }}
+                editable={true}
+                displayEventTime={false}
+                fixedWeekCount={false}
+                events={events}
+
+            />
+
+        </div>
+    );
+};
+
+export default Calendar;
