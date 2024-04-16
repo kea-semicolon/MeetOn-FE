b495ba22240567d5279fcff351bb68a632635072
은지 <81403265+eunji0714@users.noreply.github.com>
2024-04-16 10:10:09 +0900

    feat: input calendar in main

--- src/app/(route)/main/page.tsx
+++ src/app/(route)/main/page.tsx
@@ -1,9 +1,32 @@
+'use client'
+
 import Fix from '@/_components/Fix/fix'
+import Calendar from '@/_components/Chart/calendar'
+import { useEffect, useState } from 'react'
 
 export default function MainPage() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+  // Admin 컴포넌트의 mx 값을 동적으로 설정
+  const mainStyles = {
+    marginLeft: `${windowWidth / 5 - 15}px`, // 화면 너비의 1/5 만큼 왼쪽으로 이동
+  }
   return (
-    <div>
+    <div className="flex w-full">
       <Fix />
+      <div style={mainStyles} className="my-[70px]">
+        <Calendar />
+      </div>
     </div>
   )
 }
--- src/app/_components/Chart/calendar.tsx
+++ src/app/_components/Chart/calendar.tsx
@@ -1,72 +1,67 @@
 'use client'
 
-import {useState} from 'react';
-import {NextPage} from 'next';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import AddEventModal from './addEventModal';
-import '@/styles/calendar.css';
+import { useState } from 'react'
+import { NextPage } from 'next'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import AddEventModal from './addEventModal'
+import '@/styles/calendar.css'
 
 interface CalendarProps {
-    showAddButton?: boolean;
+  showAddButton?: boolean
 }
 
-const Calendar: NextPage<CalendarProps> = ({showAddButton = true}) => {
-    const [showModal, setShowModal] = useState<boolean>(false);
-    const [events, setEvents] = useState<any[]>([]);
+const Calendar: NextPage<CalendarProps> = ({ showAddButton = true }) => {
+  const [showModal, setShowModal] = useState<boolean>(false)
+  const [events, setEvents] = useState<any[]>([])
 
-    const handleSaveEvent = (newEvent: any) => {
-        setEvents(prevEvents => [...prevEvents, newEvent]);
-    };
+  const handleSaveEvent = (newEvent: any) => {
+    setEvents((prevEvents) => [...prevEvents, newEvent])
+  }
 
-    return (
-        <div className='calendar'>
-            <FullCalendar
-                plugins={[dayGridPlugin, interactionPlugin]}
-                initialView="dayGridMonth"
-                timeZone="local"
-                aspectRatio={2}
-                headerToolbar={{
-                    left : 'prev,title,next',
-                    end: 'today' + (showAddButton ? ',addEventButton' : ''),
-                }}
-                customButtons={{
-                    addEventButton: {
-                        text: '+',
-                        click: () => setShowModal(true),
-                    }
-                }}
-                eventBackgroundColor={'#FF7236'}
-                eventBorderColor={'#FF7236'}
-                eventTimeFormat={{
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    hour12: false
-                }}
-                editable={true}
-                displayEventTime={true}
-                fixedWeekCount={false}
-                events={events}
-                dayMaxEvents={2} // = eventLimit
+  return (
+    <div className="calendar w-3/5 absolute">
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        timeZone="local"
+        aspectRatio={2}
+        headerToolbar={{
+          left: 'prev,title,next',
+          end: `today${showAddButton ? ',addEventButton' : ''}`,
+        }}
+        customButtons={{
+          addEventButton: {
+            text: '+',
+            click: () => setShowModal(true),
+          },
+        }}
+        eventBackgroundColor="#FF7236"
+        eventBorderColor="#FF7236"
+        eventTimeFormat={{
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false,
+        }}
+        editable
+        displayEventTime
+        fixedWeekCount={false}
+        events={events}
+        dayMaxEvents={2} // = eventLimit
+        moreLinkClassNames={['more-events-link']}
+        moreLinkText={function (n) {
+          return `그 외 ${n}개`
+        }}
+      />
+      {showModal && (
+        <AddEventModal
+          onClose={() => setShowModal(false)}
+          onSave={handleSaveEvent}
+        />
+      )}
+    </div>
+  )
+}
 
-                moreLinkClassNames={['more-events-link']}
-                moreLinkText={
-                    function (n) {
-                        return '그 외 ' + n + '개'
-                    }
-
-                }
-
-            />
-            {showModal && (
-                <AddEventModal
-                    onClose={() => setShowModal(false)}
-                    onSave={handleSaveEvent}
-                />
-            )}
-        </div>
-    );
-};
-
-export default Calendar;
+export default Calendar
