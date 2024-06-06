a8b02c0278e4734ccbbe619d8b641b04f6bb87f7
maylh <gus_05@naver.com>
2024-06-06 18:23:20 +0900

    style: change toggle ui

--- src/app/_components/Meeting-minutes/toggle.tsx
+++ src/app/_components/Meeting-minutes/toggle.tsx
@@ -6,32 +6,32 @@ interface ToggleProps {
 
 const Toggle: React.FC<ToggleProps> = ({ onToggle }) => {
   const [isToggled, setIsToggled] = useState(false)
-  const [toggleText, setToggleText] = useState('캘린더')
 
   const handleToggle = () => {
     setIsToggled(!isToggled)
     onToggle()
-    setToggleText(isToggled ? '캘린더' : '표')
   }
 
   return (
-    <div className="flex">
+    <div className="flex items-center">
       <button
         onClick={handleToggle}
-        className={`w-[103px] h-[39px] rounded-full ${isToggled ? 'bg-[#ffcd00]' : 'bg-[#ffcd00]'} focus:outline-none relative`}
+        className="w-[97px] h-[38px] rounded-[19px] bg-[#000000] opacity-80 text-[#000000] focus:outline-none relative"
       >
         <div
-          className={`h-[34px] rounded-full transform duration-300 text-[12px] text-[#FFCD00]
-            ${isToggled ? 'translate-x-[47px] bg-white w-[53px]' : 'translate-x-[3px] bg-white w-[63px]'} 
-            flex justify-center items-center`}
+          className={`h-[30px] rounded-[15px] shadow-md transform duration-150 bg-[#ffffff] flex items-center justify-center text-[12px]
+                    ${isToggled ? 'translate-x-[48px] w-[43px]' : 'translate-x-[5px] w-[58px]'}`}
         >
-          <span>{toggleText}</span>
+          {isToggled ? '표' : '캘린더'}
         </div>
-        <span
-          className={`absolute top-1/2 transform -translate-y-1/2 text-[12px] text-[#FFF2BE] ${isToggled ? 'left-2.5' : 'right-4'}`}
-        >
-          {isToggled ? '캘린더' : '표'}
-        </span>
+        <div className="absolute inset-0 flex items-center justify-between px-2 text-[12px] text-[#959595]">
+          <span className={`${isToggled ? 'opacity-100 pl-0.5' : 'opacity-0'}`}>
+            캘린더
+          </span>
+          <span className={`${isToggled ? 'opacity-0' : 'opacity-100 pr-1.5'}`}>
+            표
+          </span>
+        </div>
       </button>
     </div>
   )
