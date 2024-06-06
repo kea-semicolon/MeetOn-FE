2374eb3cdd6c45568a01286925346c09f0efdaca
은지 <81403265+eunji0714@users.noreply.github.com>
2024-06-06 17:55:42 +0900

    refactor: modify minutes page

--- src/app/_components/Meeting-minutes/toggle.tsx
+++ src/app/_components/Meeting-minutes/toggle.tsx
@@ -18,17 +18,17 @@ const Toggle: React.FC<ToggleProps> = ({ onToggle }) => {
     <div className="flex">
       <button
         onClick={handleToggle}
-        className={`w-[103px] h-[39px] rounded-full ${isToggled ? 'bg-[#ffcd00]' : 'bg-[#ffcd00]'} focus:outline-none relative`}
+        className={`w-[103px] h-[39px] rounded-full ${isToggled ? 'bg-[#000000]' : 'bg-[#000000]'} focus:outline-none relative`}
       >
         <div
-          className={`h-[34px] rounded-full transform duration-300 text-[12px] text-[#FFCD00]
+          className={`h-[34px] rounded-full transform duration-300 text-[12px] text-[#000000]
             ${isToggled ? 'translate-x-[47px] bg-white w-[53px]' : 'translate-x-[3px] bg-white w-[63px]'} 
             flex justify-center items-center`}
         >
           <span>{toggleText}</span>
         </div>
         <span
-          className={`absolute top-1/2 transform -translate-y-1/2 text-[12px] text-[#FFF2BE] ${isToggled ? 'left-2.5' : 'right-4'}`}
+          className={`absolute top-1/2 transform -translate-y-1/2 text-[12px] text-[#959595] ${isToggled ? 'left-2.5' : 'right-4'}`}
         >
           {isToggled ? '캘린더' : '표'}
         </span>
