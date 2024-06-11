b8f7d2592874d599f88b64bc59fe9a076f8378ae
은지 <81403265+eunji0714@users.noreply.github.com>
2024-06-11 22:02:02 +0900

    feat: 메모 수정 successfully

--- src/app/_components/Memo/memoJang.tsx
+++ src/app/_components/Memo/memoJang.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import usePostMemo from '@/_hook/usePostMemo'
+import usePutMemo from '@/_hook/usePutMemo'
+import useGetMemo from '@/_hook/useGetMemo'
 
 export default function MemoJang({
   setCurrentView,
@@ -9,13 +11,40 @@ export default function MemoJang({
   selectedContent: string
 }) {
   const [memoText, setMemoText] = useState<string>('')
+  const [selectedMemoId, setSelectedMemoId] = useState<number | null>(null)
   const { mutate: createMemo } = usePostMemo()
+  const updateMemoMutation = usePutMemo()
+  const { data: Memo } = useGetMemo()
+
   useEffect(() => {
     setMemoText(selectedContent)
   }, [selectedContent])
+  useEffect(() => {
+    if (Memo?.memoList) {
+      console.log(Memo.memoList)
+      if (Memo.memoList.length > 0) {
+        setSelectedMemoId(Memo.memoList[0].memoId)
+      }
+    }
+  }, [Memo])
+
   const handleSave = () => {
     createMemo({ content: memoText })
     setCurrentView(true)
+  }
+  const handleUpdate = async () => {
+    try {
+      if (selectedMemoId !== null) {
+        await updateMemoMutation.mutateAsync({
+          memoId: selectedMemoId,
+          content: memoText,
+        })
+      } else {
+        console.error('No memoId selected for update')
+      }
+    } catch (error) {
+      console.error('Error updating memo:', error)
+    }
   }
 
   return (
@@ -26,12 +55,20 @@ export default function MemoJang({
         value={memoText}
         onChange={(e) => setMemoText(e.target.value)}
       />
-      <button
-        onClick={handleSave}
-        className="w-[66px] h-[34px] bg-black text-[#D2FA64] ml-auto rounded-[3px]"
-      >
-        저장
-      </button>
+      <div className="flex ">
+        <button
+          onClick={handleUpdate}
+          className="w-[66px] h-[34px] bg-black text-[#D2FA64] ml-auto rounded-[3px]"
+        >
+          수정
+        </button>
+        <button
+          onClick={handleSave}
+          className="w-[66px] h-[34px] bg-black text-[#D2FA64] ml-3 rounded-[3px]"
+        >
+          저장
+        </button>
+      </div>
     </div>
   )
 }
---
+++ src/app/_hook/usePutMemo.ts
@@ -0,0 +1,38 @@
+import api from '@/_service/axios'
+import { useRouter } from 'next/navigation'
+import { useMutation, MutationFunction } from '@tanstack/react-query'
+
+interface UpdateMemoProps {
+  content: string
+  memoId: number
+}
+
+const usePutMemo = () => {
+  const router = useRouter()
+  const updateMemo: MutationFunction<Response, UpdateMemoProps> = async ({
+    content,
+    memoId,
+  }) => {
+    try {
+      const response = await api.put<Response>(`/memo?memoId=${memoId}`, {
+        content,
+      })
+      console.log(response.data)
+      return response.data
+    } catch (error: any) {
+      throw new Error(`메모 수정 실패 : ${error.message}`)
+    }
+  }
+  return useMutation({
+    mutationFn: updateMemo,
+    onSuccess: (data) => {
+      console.log('메모 수정 성공')
+      router.push('/memo')
+    },
+    onError: (error: any) => {
+      console.error(error.message)
+    },
+  })
+}
+
+export default usePutMemo
